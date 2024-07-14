1320994257e143c07eec42971e1982fcccdd0f61
Yuheng Su <gipsyh.icu@gmail.com>
2024-07-14 16:27:39 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    env::temp_dir,
+    ffi::{c_char, c_void, CStr, CString},
+    mem::forget,
+};
+
+use aig::Aig;
+
+extern "C" {
+    fn Abc_FrameGetGlobalFrame() -> *mut c_void;
+    fn Cmd_CommandExecute(pAbc: *mut c_void, sCommand: *const c_char) -> i32;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct Abc {
+    ptr: *mut c_void,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Abc {
+    pub fn new() -> Self {
+        let ptr = unsafe { Abc_FrameGetGlobalFrame() };
+        assert!(!ptr.is_null(), "init abc failed");
+        Self { ptr }
+    }
+
+    pub fn execute_command(&mut self, command: &str) {
+        let c = CString::new(command).unwrap();
+        let res = unsafe { Cmd_CommandExecute(self.ptr, c.as_ptr()) };
+        assert!(res == 0, "abc execute {command} failed");
+    }
+
+    pub fn read_aig(&mut self, aig: &Aig) {
+        let tmpfile = tempfile::NamedTempFile::new().unwrap();
+        let path = tmpfile.path().as_os_str().to_str().unwrap();
+        aig.to_file(path);
+        let command = format!("read_aiger {};", path);
+        let command = CString::new(command).unwrap();
+        let res = unsafe { Cmd_CommandExecute(self.ptr, command.as_ptr()) };
+        assert!(res == 0, "abc read aig failed");
+    }
+
+    pub fn write_aig(&mut self) -> Aig {
+        let tmpfile = tempfile::NamedTempFile::new().unwrap();
+        let path = tmpfile.path().as_os_str().to_str().unwrap();
+        let command = format!("write_aiger {};", path);
+        let command = CString::new(command).unwrap();
+        let res = unsafe { Cmd_CommandExecute(self.ptr, command.as_ptr()) };
+        assert!(res == 0, "abc write aig failed");
+        Aig::from_file(path)
     }
 }
